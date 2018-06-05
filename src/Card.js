4f26c57f6c3972a3a5000a687e810355792d6a84
Zach Sotak <zs1046@txstate.edu>
2018-06-04 23:18:24 -0500

    Set up CardLIst

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id}) => {
     return(
-          <div>       {/*use robohhash api to generate random robot images*/}
-              <img alt='robots' src='https://robohash.org/test?200x200' />
-              <h2>Jane Doe</h2>
-              <p>jane.doe@gmail.com</p>
-          </div>
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
+            {/*Predefined Tachyon classes, Background, dib, border 3, padding of 3, margins 2, grow animation, bw2 and shadow-5 to add shadows, text center */}
+            <div>
+                {/*use robohhash api to generate random robot images*/}
+              <img alt='robots' src={`https://robohash.org/${id}!?200x200`} />
+                  <h2>{name}</h2>
+                  <p>{email}</p>
+            </div>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import { robots } from './robots';
+import CardList from './CardList';
 
 
 ReactDOM.render(
-    <div>
-        <Card/>
-        <Card/>
-        <Card/>
-    </div>
-
+    <CardList robots={robots}/>
     , document.getElementById('root'));
 registerServiceWorker();
