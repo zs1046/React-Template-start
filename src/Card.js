d9cc19c35048c896053bbbd430cc0140d3b3cb4d
Zach Sotak <zs1046@txstate.edu>
2018-06-04 21:11:02 -0500

    Start RoboFriend App

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+      <div>
+          <img alt='robots' src='' />
+          <h2>Jane Doe</h2>
+          <p>jane.doe@gmail.com</p>
+      </div>
+    );
+}
+
+export default Card;
