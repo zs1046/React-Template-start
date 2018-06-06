79318cc32fa314da8e990cb1d674d2ba6c9640ba
Zach Sotak <zs1046@txstate.edu>
2018-06-06 13:15:00 -0500

    Add Scroll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from './robots';
+import Scroll from "./Scroll";
 import './App.css';
 
 
@@ -9,9 +9,16 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-                robots: robots,
+                robots: [],
                 searchfield:''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => this.setState({robots: users}))
+
     }
 
     onSearchChange = (event) => {
@@ -23,13 +30,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots=>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return(
-            <div className='tc'>
-                <h1 className ='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots = {filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
---
+++ src/Scroll.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scroll = (props) =>{
+    return (
+        <div style={{overflowY: 'scroll', border: '2px solid black', height:'743px'}}>
+            {props.children}
+        </div>
+    );
+};
+
+
+
+export default Scroll;
