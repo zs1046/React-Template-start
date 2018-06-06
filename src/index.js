675463c4ddca981acaab53bab6a0ee81a2ef11a3
Zach Sotak <zs1046@txstate.edu>
2018-06-06 13:57:01 -0500

    build app and finish project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import App from './App';
+import App from './containers/App';
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
